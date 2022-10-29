ae5eee6c2c130a9cf1f6eb1db69469d18f9b3b1a
= <=>
2022-10-29 15:38:07 +0200

    add invite module

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { InviteModule } from './invite/invite.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule],
+  imports: [PrismaModule, UsersModule, AuthModule, InviteModule],
   controllers: [],
   providers: [],
 })
